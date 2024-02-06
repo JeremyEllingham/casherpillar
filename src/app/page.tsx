6725f48420579511418bab63130fc9db1d579664
Jeremy <jeremy@Admins-MacBook-Pro.local>
2024-02-06 21:06:23 +0000

    Updating home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,118 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const podcastFlipstarters = [
+    {
+      id: 1,
+      name: "Episodes 76 - 85",
+      number: 1,
+      status: "DELIVERED",
+      launched: "",
+      funded: "",
+      delivered: "",
+      askBCH: "",
+      askUSD: ""
+    },
+    {
+      id: 2,
+      name: "Episodes 86 - 95",
+      number: 2,
+      status: "DELIVERED",
+      launched: "30th July 2023",
+      funded: "1st August 2023",
+      delivered: "",
+      askBCH: "",
+      askUSD: ""
+    },
+    {
+      id: 3,
+      name: "Episodes 96 - 105",
+      number: 3,
+      status: "DELIVERED",
+      launched: "",
+      funded: "",
+      delivered: "",
+      askBCH: "",
+      askUSD: ""
+    },
+    {
+      id: 4,
+      name: "Episodes 106 - 115",
+      number: 4,
+      status: "REQUESTING",
+      launched: "",
+      funded: "",
+      delivered: "",
+      askBCH: "",
+      askUSD: ""
+    }
+  ]
+
+  const requestingFlipstarterCount = podcastFlipstarters.filter(({ status }) => status === "REQUESTING").length.toString()
+
+  const inProgressFlipstarterCount = podcastFlipstarters.filter(({ status }) => status === "IN_PROGRESS").length.toString()
+
+  const deliveredFlipstarterCount = podcastFlipstarters.filter(({ status }) => status === "DELIVERED").length.toString()
+
+  const failedFlipstarterCount = podcastFlipstarters.filter(({ status }) => status === "FAILED").length.toString()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-left p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+        <p className="text-3xl fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          CasherPillar
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+
+      <div className="flex w-full border-solid border-2 border-slate-300">
+        <div>
+          <p className="text-xl flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-red-800/30">
+            The Bitcoin Cash Podcast
+          </p>
+          <p className="text-xl flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-red-800/30">
+            ...more projects soon
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <div className="p-5">
+          <p className="text-lg">
+            The Bitcoin Cash Podcast
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p>
+            By: Jeremy
           </p>
-        </a>
+          <p className="pt-1 italic">Following Bitcoin Cash on its rise to global reserve currency. Promotion of Bitcoin Cash through regular 2 hour podcasts, music videos & informational website.</p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div>
+            <div className="mt-2 mb-2">
+              <hr />
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            <div className="justify-between">
+              <span className="px-1">
+                <a className="underline" href="https://bitcoincashpodcast.com" target="_blank" >bitcoincashpodcast.com</a>
+              </span>
+              <span className="px-1">
+                <a className="underline" href="https://twitter.com/TheBCHPodcast" target="_blank" >Twitter</a>
+              </span>
+            </div>
+
+            <div className="mt-2 mb-2">
+              <hr />
+            </div>
+          </div>
+
+          <p className="text-lg">FUNDING</p>
+
+          <p>Last Funded - x</p>
+          <p>Flipstarters ({podcastFlipstarters.length.toString()}) - Requesting Funding ({requestingFlipstarterCount}) - In Progress ({inProgressFlipstarterCount}) - Delivered ({deliveredFlipstarterCount}) - Failed ({failedFlipstarterCount})</p>
+          <p>
+            #1:
           </p>
-        </a>
+          <p>
+            Launched: x/x/x - x/x/x: Delivered
+          </p>
+        </div>
       </div>
     </main>
   );
